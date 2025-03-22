f26e83dc977ec0f5cee86ff1e8379caaff7e125f
RakaValrizqy <pendragonqword@gmail.com>
2025-03-22 22:26:04 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_103022330096/Program.cs
+++ tpmodul6_103022330096/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_103022330096;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo stv = new SayaTubeVideo("Tutorial Design By Contract - Raka Valrizqy Akhdansyah");
+        stv.IncreasePlayCount(1000);
+        stv.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_103022330096/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022330096
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        Random rand = new Random();
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.id = rand.Next(10000,99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int jumlah)
+        {
+            this.playCount += jumlah;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($" ID: {id}, Title: {title}, PlayCount: {playCount}");
+        }
+    }
+
+    
+}
